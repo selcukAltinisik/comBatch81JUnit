593f623d69cbe02bc4c5f6856b670c56250c7802
selcukAltinisik <selcukaltin3544@gmail.com>
2022-09-20 01:32:34 +0300

    20Eyl22-01:32

--- src/test/java/day08/C04_Iframe.java
+++ src/test/java/day08/C04_Iframe.java
@@ -1,4 +1,47 @@
 package day08;
 
-public class C04_Iframe { 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class C04_Iframe {
+
+    WebDriver driver;
+    @Before
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @After
+    public void tearDown() throws InterruptedException {
+
+        driver.close();
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+        driver.get("https://html.com/tags/iframe/");
+        List<WebElement> iframeList = new ArrayList<>(driver.findElements(By.xpath("//iframe")));
+        driver.switchTo().frame(iframeList.get(0));
+        WebElement youTubePlayButton = driver.findElement(By.xpath("//*[@aria-label='Oynat']"));
+        Thread.sleep(3000);
+        youTubePlayButton.click();
+    }
+
+
+
+
+
 }
