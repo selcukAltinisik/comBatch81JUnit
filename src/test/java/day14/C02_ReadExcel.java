367e2870fa002199170bbc9c503011179854ae60
Selçuk Altınışık <selcukaltin3544@gmail.com>
2022-09-26 12:37:54 +0300

    Update C02_ReadExcel.java

--- src/test/java/day14/C02_ReadExcel.java
+++ src/test/java/day14/C02_ReadExcel.java
@@ -21,7 +21,7 @@ public class C02_ReadExcel {
         //- Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
         //- WorkbookFactory.create(fileInputStream)
         Workbook workbook = WorkbookFactory.create(fis); //Workbook objesiyle fis oblesi ile akışa aldığımız dosyamızla bir excell dosyası create ettik
-        //- Sheet objesi olusturun workbook.getSheetAt(index)
+        //-- Sheet objesi olusturun workbook.getSheetAt(index)
         //- Row objesi olusturun sheet.getRow(index)
         //- Cell objesi olusturun row.getCell(index)
         String actualData = workbook.getSheet("Sayfa1").getRow(3).getCell(3).toString();
